bbec6150102c0a3eaa4c8d15d026b325a49e9769
MICHADES22 <desmichael2@gmail.com>
2026-06-08 22:15:19 +0530

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,22 +1,21 @@
 import socket
-from concurrent.futures import ThreadPoolExecutor
 
-target = "scanme.nmap.org"
+target = "scname.nmap.org"
 start_port = 1
 end_port = 1024
-
-def scan_port(port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(0.5)
-    if s.connect_ex((target, port)) == 0:
-        print(f"[+] Port {port} is OPEN")
-    s.close()
-
 
 print(f"\nScanning Target: {target}")
 print("-" * 40)
 
-with ThreadPoolExecutor(max_workers=100) as executor:
-    executor.map(scan_port, range(start_port, end_port))
+for port in range(start_port, end_port + 1):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.5)
+
+    result = s.connect_ex((target, port))
+
+    if result == 0:
+        print(f"[+] Port {port} is OPEN")
+
+    s.close()
 
 print("\nScan Completed.")
